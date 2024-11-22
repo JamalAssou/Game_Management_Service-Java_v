93eaae396d539f97afc257c27b3a286d0de678dc
JamalAssou <125282202+JamalAssou@users.noreply.github.com>
2024-11-22 09:22:26 +0100

    :feat initial commit

---
+++ Game Management Service/src/main/java/org/example/game_management_service/GameManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.game_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameManagementServiceApplication.class, args);
+    }
+
+}
---
+++ Game Management Service/src/test/java/org/example/game_management_service/GameManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.game_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
